6c1ea9e918b283b0fa1f86411f4a62750e6edf62
EzequielGaribotto <ezequiel.garibotto.7e7@itb.cat>
2025-05-26 13:35:00 +0200

    fix deploy

---
+++ src/config/cvMetadata.ts
@@ -0,0 +1,10 @@
+export const cvMetadata = {
+  spanish: {
+    lastUpdated: "2025-01-02",
+    filename: "CV_Ezequiel_Garibotto_ES.pdf"
+  },
+  english: {
+    lastUpdated: "2025-01-02", 
+    filename: "CV_Ezequiel_Garibotto_EN.pdf"
+  }
+};
